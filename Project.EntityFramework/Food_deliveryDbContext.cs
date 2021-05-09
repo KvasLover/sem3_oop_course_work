23ac6d242297aa2855d3ca3882182cd653ef6f61
Вадим Курбатский <vadimkurb22@gmail.com>
2021-05-09 15:35:48 +0300

    last

---
+++ Project.EntityFramework/Food_deliveryDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Food_delivery.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.EntityFramework
+{
+    public class Food_deliveryDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<>().OwnsOne()
+            base.OnModelCreating(modelBuilder);
+        }*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=VADIM\\MSSQLSERVER_VADI;DataBase=Food_delivery;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
