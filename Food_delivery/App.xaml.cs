006c02c18889243bc0638d239f6f21828566c811
Вадим Курбатский <vadimkurb22@gmail.com>
2021-05-09 12:04:26 +0300

    27:53. Какая-то фигня с MainWindow.xaml, биндинг навигатора

--- Food_delivery/App.xaml.cs
+++ Food_delivery/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Food_delivery.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -16,6 +17,7 @@ namespace Food_delivery
         protected override void OnStartup(StartupEventArgs e)
         {
             Window window = new MainWindow();
+            window.DataContext = new MainViewModel();
             window.Show();
 
             base.OnStartup(e);
--- Food_delivery/State/Navigators/Navigator.cs
+++ Food_delivery/State/Navigators/Navigator.cs
@@ -2,6 +2,7 @@
 using Food_delivery.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,27 @@ using System.Windows.Input;
 
 namespace Food_delivery.State.Navigators
 {
-    class Navigator : INavigator
+    class Navigator : INavigator, INotifyPropertyChanged
     {
-        public ViewModelBase CurrentViewModel { get; set; }
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+            }
+        }
 
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
